32a64b9811f93487540ed0154ba81971da90032e
Parthiv Kheni <Parthiv.Kheni@einfochips.com>
2023-08-22 11:59:06 +0530

    ---------------------------> 1 <----------------------------------

---
+++ demo.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+void main()
+{
+	int n ;
+
+	printf("heyy   how   are  you....\n");
+	
+	scanf("%d",&n);
+
+	printf("\n\nThanks.... num = %d");
+}
